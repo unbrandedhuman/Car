acf32032d2bfe9e15a3023a0d512ac3224097b7f
Matthew Smith <matthew.hayden.smith@icloud.com>
2024-01-05 18:06:17 -0500

    Added Maintenance card.

---
+++ Car/Views/Detail Views/Cars/MaintenanceRecordCard.swift
@@ -0,0 +1,18 @@
+//
+//  MaintenanceRecordCard.swift
+//  Car
+//
+//  Created by Matthew Smith on 1/5/24.
+//
+
+import SwiftUI
+
+struct MaintenanceRecordCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MaintenanceRecordCard()
+}
